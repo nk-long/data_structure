f3b92e0d1c3bb02078abae0bb07ab2f58473390e
nk-long <lambenitin@gmail.com>
2021-08-29 09:58:18 +0530

    Recursive function example

---
+++ Percentage.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Percentage {
+    static int count =1;
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int num = sc.nextInt();
+       
+
+       percentage(num);
+
+        
+    }
+
+    static void percentage(int num) {
+        float res=0 ;
+        if(count==10){
+            
+            return;
+        }
+        res=((float)count/num)*100;
+            System.out.println(count +" percent is "+res);
+            count++;
+            percentage(num);
+
+    }
+}
